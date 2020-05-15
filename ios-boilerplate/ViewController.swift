8cd4e1e581cd6a86107f1df1639dd2fcd87bd39d
Firat Yenidunya <firat.yenidunya@adesso.com.tr>
2020-05-15 16:42:34 +0300

    creating xcode project and initial structure

---
+++ ios-boilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-boilerplate
+//
+//  Created by Firat Yenidunya on 15.05.2020.
+//  Copyright © 2020 com.adesso.turkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
