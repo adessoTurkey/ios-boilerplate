3429b5f66416818bd46f416c695f40985bc5f9a1
Baybunar Can, ENT-WSC-SZE-ALR (EXT) <Can.Baybunar@swisscom.com>
2020-11-25 11:20:27 +0300

    Some common extensions added

---
+++ ios-boilerplate/Utility/Extensions/UITableViewExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  UITableViewExtension.swift
+//  ios-boilerplate
+//
+//  Copyright © 2020 Adesso Turkey. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+
+    func dequeue<T: UITableViewCell>(withIdentifier identifier: String = String(describing: T.self)) -> T {
+        guard let cell = self.dequeueReusableCell(withIdentifier: identifier) as? T
+            else { fatalError("Could not dequeue cell with identifier \(identifier) from tableView \(self)") }
+        return cell
+    }
+
+    func dequeue<T: UITableViewCell>(withIdentifier identifier: String = String(describing: T.self), at indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? T
+            else { fatalError("Could not dequeue cell with identifier \(identifier) from tableView \(self)") }
+        return cell
+    }
+    
+    func dequeueHeaderFooterView<T: UITableViewHeaderFooterView>(withIdentifier identifier: String = String(describing: T.self)) -> T {
+        guard let cell = self.dequeueReusableHeaderFooterView(withIdentifier: identifier) as? T else {
+            fatalError("Could not dequeue cell with identifier \(identifier) from tableView \(self)") }
+        return cell
+    }
+}
