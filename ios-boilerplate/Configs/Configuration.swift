a86bc708a7675960b842ff49f869203e4dfb2609
Ulug, Baha <Baha.Ulug@adesso.com.tr>
2020-12-01 19:58:54 +0300

    * Configuration file is added for run time check. * Pod warnings are resolved.

---
+++ ios-boilerplate/Configs/Configuration.swift
@@ -0,0 +1,37 @@
+//
+//  Configuration.swift
+//  ios-boilerplate
+//
+//  Created by Baha Ulug on 1.12.2020.
+//  Copyright © 2020 Adesso Turkey. All rights reserved.
+//
+
+import Foundation
+
+final class Configuration {
+
+    static var isProduction: Bool {
+        #if Production
+            return true
+        #else
+            return false
+        #endif
+    }
+
+    static var isAppStore: Bool {
+        #if AppStore
+            return true
+        #else
+            return false
+        #endif
+    }
+
+    static var isDevelopment: Bool {
+        #if Development
+            return true
+        #else
+            return false
+        #endif
+    }
+
+}
