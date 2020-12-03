b1dec595c997622f653e9c18e332eaf9cf36159f
Ulug, Baha <Baha.Ulug@adesso.com.tr>
2020-12-03 09:38:16 +0300

    * private access level is added for services * mock data file name changed. * pod file is edited

--- ios-boilerplate/Application/AppDelegate.swift
+++ ios-boilerplate/Application/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    let services: [UIApplicationDelegate] = [SwifterServerService()]
+    private let services: [UIApplicationDelegate] = [SwifterServerService()]
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
