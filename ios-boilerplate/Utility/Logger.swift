0c58d0dcfad8fff1a0c825b4ef605b6ce6c27687
ismail <iso.kilic@hotmail.com>
2020-12-22 10:43:29 +0300

    Swift rules edited.  Warnings cleared.

--- ios-boilerplate/Utility/Logger.swift
+++ ios-boilerplate/Utility/Logger.swift
@@ -19,17 +19,17 @@ enum LogLevel: Int, CustomStringConvertible {
 
     var description: String {
         switch self {
-            case .verbose:
-                return "Verbose"
-            case .debug:
-                return "Debug"
-            case .info:
-                return "Info"
-            case .warning:
-                return "Warning"
-            case .error:
-                return "Error"
-            }
+        case .verbose:
+            return "Verbose"
+        case .debug:
+            return "Debug"
+        case .info:
+            return "Info"
+        case .warning:
+            return "Warning"
+        case .error:
+            return "Error"
+        }
     }
 }
 
@@ -69,16 +69,16 @@ final class Logger {
                                  line: line)
 
         switch level {
-            case .verbose:
-                DDLogVerbose(message.description)
-            case .debug:
-                DDLogDebug(message.description)
-            case .info:
-                DDLogInfo(message.description)
-            case .warning:
-                DDLogWarn(message.description)
-            case .error:
-                DDLogError(message.description)
+        case .verbose:
+            DDLogVerbose(message.description)
+        case .debug:
+            DDLogDebug(message.description)
+        case .info:
+            DDLogInfo(message.description)
+        case .warning:
+            DDLogWarn(message.description)
+        case .error:
+            DDLogError(message.description)
         }
     }
 
