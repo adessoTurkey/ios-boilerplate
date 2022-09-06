65d903069071227a738eff380e4093d685b8c22e
adesso-said <said.alir@adesso.com.tr>
2022-09-06 12:32:42 +0300

    bash modified

--- scripts/installation/swiftlint.sh
+++ scripts/installation/swiftlint.sh
@@ -2,10 +2,7 @@ if test ! $(which brew); then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
 # Update homebrew recipes
 brew update
-
 brew install swiftlint
-
-echo "Ready to go!..."
+echo "Ready to go!.."
