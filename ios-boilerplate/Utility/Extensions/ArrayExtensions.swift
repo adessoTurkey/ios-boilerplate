e265ed30b28c90b10520b67146efa1643bfbf9c6
Ege Sucu <Ege.sucu@adesso.com.tr>
2023-04-10 11:53:50 +0300

    [BS-0001] Added Array extension
    to support CoreData array objects.

---
+++ ios-boilerplate/Utility/Extensions/ArrayExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  ArrayExtensions.swift
+//  ios-boilerplate
+//
+//  Created by Sucu, Ege on 10.04.2023.
+//  Copyright © 2023 Adesso Turkey. All rights reserved.
+//
+
+import Foundation
+
+extension Array: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Element].self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
